3707e4b7706391648de7766bdf72c07fc66a467a
Niranjana <ponniranjana2006@gmail.com>
2026-06-05 10:29:17 +0530

    Create C_Problem_3.c

---
+++ C_Problem_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int i,sum;
+    i=0;
+    sum=0;
+    loop:if(i<5)
+    {
+        i=i+1;
+        sum=sum+i;
+        goto loop;
+    }
+    printf("%d",sum);
+    return 0;
+}
