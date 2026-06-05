cafe00d22057f9471a20f7d5ff2f63a7b9e8c44e
Niranjana <ponniranjana2006@gmail.com>
2026-06-05 22:50:01 +0530

    Create C_Problem_8.c

---
+++ C_Problem_8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n, tens, ones, sum;
+    n=10;
+loop:if(n < 100)
+    {
+        ones=n%10;
+        tens=n/10;
+        sum=tens+ones;
+        if(sum==6)
+        {
+            printf("%d\n", n);
+        }
+        n=n+2;
+        goto loop;
+    }
+    return 0;
+}
