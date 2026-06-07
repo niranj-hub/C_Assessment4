59ca56e28248531ceaf38b9cb5a07f11aa8a754f
Niranjana <ponniranjana2006@gmail.com>
2026-06-07 10:44:01 +0530

    Create C_Problem_14.c

--- C_Problem_14.c
+++ C_Problem_14.c
@@ -5,16 +5,16 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &n);
     divisor = 1; 
-    last = n % 10;
-    temp = n;
+    last=n%10;
+    temp=n;
     loop:if(temp >= 10)
     {
-        temp = temp / 10;
-        divisor = divisor * 10;
+        temp=temp/10;
+        divisor=divisor*10;
         goto loop;
     }
-    first = temp;
-    middle = (n % divisor) / 10;
+    first=temp;
+    middle=(n % divisor)/10;
     result = (last * divisor) + (middle * 10) + first;
     printf("Result = %d", result);
     return 0;
